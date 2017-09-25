cc234ec82d2d74ec4bad9feb15bf3b030992c930
Bryan Ferris <saffsnail@gmail.com>
2017-09-25 08:30:15 -0700

    Improved the style of setup.scm; functionality is unchanged

--- setup.scm
+++ setup.scm
@@ -1,3 +1,7 @@
-(safe-mkdir (in-home '(".config")))
-(safe-symlink (in-dotdir '("nvim")) (in-home '(".config/nvim")))
+(let ((configname (in-home '(".config"))))
+  (unless (access? configname F_OK)
+    (mkdir configname)
+  )
+  (safe-symlink (in-dotdir '("nvim")) (string-append configname "/nvim"))
+)
 
